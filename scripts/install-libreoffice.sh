56a3960018af9b22fb52415429389bd101343211
Markus Rippl <m-kontakt@rippl.name>
2019-06-02 16:43:07 +0200

    add all fonts before installing CODE

--- scripts/install-libreoffice.sh
+++ scripts/install-libreoffice.sh
@@ -9,6 +9,17 @@ apt-get -y install apt-transport-https
 # Install locales
 apt-get -y install locales-all
 
+# Install MS TTF fonts
+echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
+# MS TTF
+apt-get install --yes --no-install-recommends ttf-mscorefonts-installer curl
+# Cleartype fonts
+mkdir ~/.fonts
+mkdir /usr/share/fonts/msttvistafonts
+curl http://plasmasturm.org/code/vistafonts-installer/vistafonts-installer | bash
+cp ~/.fonts/* /usr/share/fonts/msttvistafonts/
+fc-cache
+
 # Add Collabora repos
 echo "deb https://collaboraoffice.com/repos/CollaboraOnline/CODE /" > /etc/apt/sources.list.d/collabora.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 6CCEA47B2281732DF5D504D00C54D189F4BA284D
@@ -19,10 +30,6 @@ apt-get -y install loolwsd code-brand collaboraoffice6.0-dict* collaboraofficeba
 
 # Install inotifywait and killall to automatic restart loolwsd, if loolwsd.xml changes
 apt-get -y install inotify-tools psmisc
-
-# Install MS TTF fonts
-echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
-apt-get install --yes --no-install-recommends ttf-mscorefonts-installer
 
 # Cleanup
 rm -rf /var/lib/apt/lists/*
